40a8485b7c6d5d14b3f1cb756ef515cd4570c4f0
cmatis2 <87940509+cmatis2@users.noreply.github.com>
2022-12-22 14:40:35 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.ReviveEvent = 'esx_ambulancejob:revive' -- The event used to revive people (default: esx_ambulancejob:revive)
+				
+--Webhook settings
+Config.Webhook = 'yourwebhookhere' -- Reports error messages in commands
+Config.WebhookTitle = 'Liberty RP' -- Title for webhook
+Config.WebhookFooter = 'made by Melktert' -- Footer of webhook
+--Embed colors
+Config.Success = 3145631 --
+Config.Error = 16711680 -- https://www.mathsisfun.com/hexadecimal-decimal-colors.html (decimal colors)
+Config.Neutral = 9606296 -- 
+		
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Melktert'
+description 'Discord2FivePY'
+version '1.0.0'
+
+server_scripts {
+	'server/server.lua',
+	'config.lua'
+}
